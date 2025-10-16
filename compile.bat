237c81d4bb4db677b5135eb36ec0d2be842bd67e
nitzryan <nitzryan@gmail.com>
2025-10-16 17:19:32 -0500

    Added Crystal Diamond

--- compile.bat
+++ compile.bat
@@ -1,4 +1,5 @@
 python html_editor.py index.html 
+python html_editor.py CrystalDiamond.html html/
 python html_editor.py Hex.html html/
 python html_editor.py PathPlanning.html html/
 python html_editor.py PortfolioSim.html html/
--- js/header.js
+++ js/header.js
@@ -5,6 +5,7 @@ console.log(path)
 let nav_home = document.getElementById('nav_home')
 
 let nav_ai = document.getElementById('nav_ai')
+let nav_ai_baseball = document.getElementById('nav_ai_baseball')
 let nav_ai_hex = document.getElementById('nav_ai_hex')
 let nav_ai_portsim = document.getElementById('nav_ai_portsim')
 
@@ -15,6 +16,10 @@ let nav_sim_smoke = document.getElementById('nav_sim_smoke')
 if (path == "/")
 {
     nav_home.classList.add('selected')
+} else if (path.includes('CrystalDiamond.html'))
+{
+    nav_ai.classList.add('selected')
+    nav_ai_baseball.classList.add('selected')
 } else if (path.includes("Hex.html"))
 {
     nav_ai.classList.add('selected')
